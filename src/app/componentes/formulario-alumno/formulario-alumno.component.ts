283d6e119b74f1890c038ae7db58427cbf89d743
nicolasCis <nico.cisterna@duocuc.cl>
2022-09-14 15:34:17 -0300

    trabaje formulario alumno

--- src/app/componentes/formulario-alumno/formulario-alumno.component.ts
+++ src/app/componentes/formulario-alumno/formulario-alumno.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input} from '@angular/core';
+import {Alumno, Seccion} from '../../modelo/alumno';
 @Component({
   selector: 'app-formulario-alumno',
   templateUrl: './formulario-alumno.component.html',
   styleUrls: ['./formulario-alumno.component.scss']
 })
-export class FormularioAlumnoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class FormularioAlumnoComponent {
+  @Input() public alumno !: Array<Alumno>;
 
 }
--- src/app/modelo/alumno.ts
+++ src/app/modelo/alumno.ts
@@ -1,2 +1,9 @@
+export type Seccion = 'BaseDeDatos' |'Programacion'
+
 export interface Alumno {
+  rutAlumno:Number,
+  nombreAlumno:String,
+  apellidoAlumno:String,
+  edadAlumno:Number,
+  seccionAlumno:Seccion
 }
